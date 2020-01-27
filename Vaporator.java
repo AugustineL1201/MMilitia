04a5feb6416222063beca06e8743a342ed4360eb
AugustineL1201 <60231579+AugustineL1201@users.noreply.github.com>
2020-01-27 12:35:17 -0500

    Update Vaporator.java

--- Vaporator.java
+++ Vaporator.java
@@ -2,7 +2,15 @@ package lectureplayer;
 import battlecode.common.*;
 
 public class Vaporator extends Building {
+    static int numVaporators = 0;
     public Vaporator(RobotController r) {
         super(r);
     }
+    
+    if(numVaporators < 2) && (numMiners == 7) && (numLandscapers == 4) {
+            for (Direction dir : Util.directions)
+                if(tryBuild(RobotType.VAPORATOR, dir)){
+                    numMiners++;
+                }
+        }
 }
