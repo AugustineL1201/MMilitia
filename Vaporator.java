928064df5ad2f792d5f61e7855e988be5ceccef4
AugustineL1201 <60231579+AugustineL1201@users.noreply.github.com>
2020-01-27 13:51:35 -0500

    Update Vaporator.java

--- Vaporator.java
+++ Vaporator.java
@@ -1,8 +1,7 @@
-package lectureplayer;
+package MMilitia;
 import battlecode.common.*;
 
 public class Vaporator extends Building {
-    static int numVaporators = 0;
     public Vaporator(RobotController r) {
         super(r);
     }
