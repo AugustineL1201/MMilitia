354683ee5730d5eb6d487e25badb3992c5c42071
AugustineL1201 <60231579+AugustineL1201@users.noreply.github.com>
2020-01-27 12:35:10 -0500

    Update DesignSchool.java

--- DesignSchool.java
+++ DesignSchool.java
@@ -2,6 +2,8 @@ package lectureplayer;
 import battlecode.common.*;
 
 public class DesignSchool extends Building {
+    static int numLandscapers = 0;
+    
     public DesignSchool(RobotController r) {
         super(r);
     }
@@ -11,11 +13,13 @@ public class DesignSchool extends Building {
         
         // will only actually happen if we haven't already broadcasted the creation
         comms.broadcastDesignSchoolCreation(rc.getLocation());
-
+        
+        if (numLandscapers < 12) {
         for (Direction dir : Util.directions) {
             if(tryBuild(RobotType.LANDSCAPER, dir)) {
                 System.out.println("made a landscaper");
             }
+        }
         }
     }
 }
