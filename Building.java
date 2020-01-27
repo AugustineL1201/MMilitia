3895bf500614fabc02a492e04b888fc6397cfefa
AugustineL1201 <60231579+AugustineL1201@users.noreply.github.com>
2020-01-27 14:32:14 -0500

    Update Building.java

--- Building.java
+++ Building.java
@@ -2,10 +2,18 @@ package MMilitia;
 import battlecode.common.*;
 
 public class Building extends Robot {
-
+    
+static int numVaporators = 0;
+    
     public Building(RobotController r) {
         super(r);
         // building specific setup here
+        if ((numVaporators < 2) && (HQ.numMiners == 7) && (DesignSchool.numLandscapers == 4)) {
+            for (Direction dir : Util.directions)
+                if(tryBuild(RobotType.VAPORATOR, dir)){
+                    numVaporators++;
+                }
+        }
     }
 
     public void takeTurn() throws GameActionException {
