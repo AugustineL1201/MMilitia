0ba8d9dd5c1401cf0cb20740547c09b5e2677686
AugustineL1201 <60231579+AugustineL1201@users.noreply.github.com>
2020-01-27 20:14:10 -0500

    Update Building.java

--- Building.java
+++ Building.java
@@ -3,8 +3,6 @@ import battlecode.common.*;
 
 public class Building extends Robot {
     
-static int numVaporators = 0;
-    
     public Building(RobotController r) {
         super(r);
         // building specific setup here
