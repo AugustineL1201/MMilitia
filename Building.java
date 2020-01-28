9ac1bf2b8a802f9a93dfb983d14d55b3055e2973
AugustineL1201 <60231579+AugustineL1201@users.noreply.github.com>
2020-01-27 20:12:44 -0500

    Update Building.java

--- Building.java
+++ Building.java
@@ -8,12 +8,6 @@ static int numVaporators = 0;
     public Building(RobotController r) {
         super(r);
         // building specific setup here
-        if ((numVaporators < 2) && (HQ.numMiners == 7) && (DesignSchool.numLandscapers == 4)) {
-            for (Direction dir : Util.directions)
-                if(tryBuild(RobotType.VAPORATOR, dir)){
-                    numVaporators++;
-                }
-        }
     }
 
     public void takeTurn() throws GameActionException {
