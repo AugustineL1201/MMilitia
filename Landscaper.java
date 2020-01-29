34297e4f65bf74032087e8e1aa8851c41da8d9d8
AugustineL1201 <60231579+AugustineL1201@users.noreply.github.com>
2020-01-28 22:22:05 -0500

    Update Landscaper.java

--- Landscaper.java
+++ Landscaper.java
@@ -28,7 +28,7 @@ public class Landscaper extends Unit {
             int lowestElevation = 9999999;
             for (Direction dir : Util.directions) {
                 MapLocation tileToCheck = hqLoc.add(dir);
-                if(rc.getLocation().distanceSquaredTo(tileToCheck) < 1
+                if(rc.getLocation().distanceSquaredTo(tileToCheck) < 3
                         && rc.canDepositDirt(rc.getLocation().directionTo(tileToCheck))) {
                     if (rc.senseElevation(tileToCheck) < lowestElevation) {
                         lowestElevation = rc.senseElevation(tileToCheck);
